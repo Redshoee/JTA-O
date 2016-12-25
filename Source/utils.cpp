58d3297007e472c01c3c58be61694fddaad38c67
electricman226 <electricman226@gmail.com>
2016-12-25 02:45:06 -0500

    Moved around some functions

--- Source/utils.cpp
+++ Source/utils.cpp
@@ -9,3 +9,26 @@ bool BoolDefToBool(BOOL b)
 {
 	return b == 1;
 }
+
+std::vector<std::string> Split(std::string str, std::string sep) {
+	char* cstr = const_cast<char*>(str.c_str());
+	char* current;
+	std::vector<std::string> arr;
+	current = strtok(cstr, sep.c_str());
+	while (current != NULL) {
+		arr.push_back(current);
+		current = strtok(NULL, sep.c_str());
+	}
+	return arr;
+}
+
+void ReplaceAll(std::string& str, const std::string& from, const std::string& to)
+{
+	if (from.empty())
+		return;
+	size_t start_pos = 0;
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length();
+	}
+}
